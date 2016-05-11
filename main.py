772bd8e98aca200523fbd253cfe0cd92f1436606
AndreyNoskov <noskoff.andrey@gmail.com>
2016-05-11 17:19:55 +0300

    Rule generation behavior now can be defined from config file

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import time
 from misc import about_times
 from plotter import plot_time_per_rule
 
-
-with open('config.json') as config_file:
-    config = json.load(config_file)
+try:
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+except FileNotFoundError:
+    print("Can not found config file")
+    exit()
 
 generator = Generator(config)
 sender = Sender(config)
@@ -22,20 +25,26 @@ pp = pprint.PrettyPrinter(indent=4)
 
 installed_counter = 0
 deleted_counter = 0
-number_of_rules = 1
+number_of_rules = 0
+from_file = config["rule_generator"]["parameters"]["from_file"]
+if from_file:
+    log_name = config["rule_generator"]["parameters"]["file_name"]
+    try:
+        rule_list = generator.create_from_file(log_name)
+        number_of_rules = len(rule_list)
+    except FileNotFoundError:
+        print("Can not find file " + log_name)
+        exit()
+else:
+    number_of_rules = config["rule_generator"]["parameters"]["number_of_rules"]
 times = []
-log_name = "./rules_log/20160511-145128.txt"
-try:
-    rule_list = generator.create_from_file(log_name)
-except FileNotFoundError:
-    print("Can not find file " + log_name)
+
 
 time_str = time.strftime("%Y%m%d-%H%M%S")
 with open('./rules_log/' + time_str + '.txt', 'w') as log_file:
     for i in range(number_of_rules):
         startTime = time.time()
-        # rule = generator.create_rule()
-        rule = rule_list[i]
+        rule = rule_list[i] if from_file else generator.create_rule()
         add_ins, add_del = sender.send(rule)
         installed_counter += add_ins
         deleted_counter += add_del
